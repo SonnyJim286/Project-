de430786f065dbf1ef6edfd10e63602bc137b4bb
SonnyJim286 <66345765+SonnyJim286@users.noreply.github.com>
2020-08-10 09:46:04 +1000

    little testing

--- Testing.py
+++ Testing.py
@@ -1,7 +1,22 @@
 import csv
+import rhinoscriptsyntax as rs
 
-with open('employee_file.csv', mode='w') as employee_file:
-    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    employee_writer.writerow(['John Smith', 'Accounting', 'November'])
-    employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
+def CSVbuilding():
+    #prompt the user for a file to import
+    filter = "CSV file (Subject.csv)
+    filename = rs.OpenFileName("Open Point File", filter)
+    if not filename: return
+
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row['Use'], row['Square Footage'])
+#            print(row)
+
+##########################################################################
+# Check to see if this file is being executed as the "main" python
+# script instead of being used as a module by some other python script
+# This allows us to use the module which ever way we want.
+if( __name__ == "__main__" ):
+    CSVbuilding()
